c5d7d8169fb93cb6cf661ae95bd118106a282695
anushadv99 <97237876+anushadv99@users.noreply.github.com>
2022-01-28 13:12:09 +0530

    First Commit

---
+++ AP_Servlet_Phase2/src/com/formFieldsTracking/Dashboard3.java
@@ -0,0 +1,50 @@
+package com.formFieldsTracking;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Dashboard3
+ */
+public class Dashboard3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Dashboard3() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        String userId = request.getParameter("userid");
+        if (userId == null) {
+            out.println("No UserId was found in hidden form field.<br>");
+        } else {
+            out.println("UserId found in hidden form field: " + userId + "<br>");
+            
+        }
+        out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
